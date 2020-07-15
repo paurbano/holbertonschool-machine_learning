b386a09830c3ec3f8b5f802e4d4d8bdc9904ace2
Pablo <paurbano@gmail.com>
2020-07-15 20:54:00 +0000

    adds two arrays element-wise

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' '''
+
+def add_arrays(arr1, arr2):
+    '''sum two arrays '''
+    array = []
+    if len(arr1) != len(arr2):
+        return None
+
+    for i in range(len(arr1)):
+        array.append(arr1[i] + arr2[i])
+
+    return array
