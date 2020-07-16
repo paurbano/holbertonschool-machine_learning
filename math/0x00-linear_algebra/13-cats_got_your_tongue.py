aebede01eaa5f3b7be926859427819e15fc92d3c
Pablo <paurbano@gmail.com>
2020-07-16 04:56:56 +0000

    concatenate tow matrices along axis with numpy

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    ''' concatenates two matrices'''
+    matrix = np.concatenate((mat1, mat2), axis)
+    return matrix
