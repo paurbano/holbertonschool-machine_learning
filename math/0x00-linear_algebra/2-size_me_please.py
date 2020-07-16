6a6ca87a7278c53122e3baf13c90883ddcf4a8f0
Pablo <paurbano@gmail.com>
2020-07-16 18:21:49 +0000

    function that calculates the shape of a matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -5,11 +5,21 @@
 def matrix_shape(matrix):
     ''' function that calculates the shape of a matrix'''
     shape = []
+
+    if not isinstance(matrix, list):
+        return None
+
     if matrix is None or len(matrix) == 0:
         return shape
 
-    shape.append(len(matrix))
-    shape.append(len(matrix[0]))
-    if isinstance(matrix[0][0], list):
-        shape.append(len(matrix[0][0]))
-    return shape
+    if isinstance(matrix[0], list):
+        shape.append(len(matrix))
+        shape.append(len(matrix[0]))
+        if isinstance(matrix[0][0], list):
+            shape.append(len(matrix[0][0]))
+        return shape
+    elif isinstance(matrix[0], int):
+        shape.append(len(matrix))
+        return shape
+    else:
+        return None
