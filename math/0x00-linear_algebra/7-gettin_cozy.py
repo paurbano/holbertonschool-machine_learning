42795177c2f849bc9eb3c09d98a5091881bbf2e5
Pablo <paurbano@gmail.com>
2020-07-16 23:38:02 +0000

    concatenate two matrices along axis

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,22 +5,22 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     ''' concatenate two matrices along specific axis '''
     # rows matrix 1
-    m = len(mat1)
+    m1 = len(mat1)
     # columns matrix 1
-    n = len(mat1[0])
+    n1 = len(mat1[0])
     # rows matrix 2
-    r = len(mat2)
+    m2 = len(mat2)
     # columns matrix 2
-    c = len(mat2[0])
+    n2 = len(mat2[0])
     # new matrix
     matrix = []
     # make a copy of data
     a = [row[:] for row in mat1]
     b = [row[:] for row in mat2]
-    if (axis == 1) and (m == r):
-        for i in range(m):
+    if (axis == 1) and (m1 == m2):
+        for i in range(m1):
             matrix.append(a[i] + b[i])
-    elif (axis == 0) and (n == c):
+    elif (axis == 0) and (n1 == n2):
         matrix = a + b
     else:
         return None
