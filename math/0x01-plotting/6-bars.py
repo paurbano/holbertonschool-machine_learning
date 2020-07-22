28272c6f8b67fd16798791c7312ca1d951aca9eb
Pablo <paurbano@gmail.com>
2020-07-22 20:55:12 +0000

    plot stacked bar graph

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -1,30 +1,45 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
-# print(fruit)
+fruit = np.random.randint(0, 20, (4, 3))
 
-# your code here
-labels = ['Farrah', 'Fred', 'Felicia']
-width = 0.5
+# y-axis in bold
+# rc('font', weight='bold')
+
+# Values of each group
 apples = fruit[0].copy()
 bananas = fruit[1].copy()
 oranges = fruit[2].copy()
 peaches = fruit[3].copy()
 
-fig, ax = plt.subplots()
-ax.bar(labels, apples, width, label='apples', color='red')
-ax.bar(labels, bananas, width, label='bananas', bottom=apples, color='yellow')
-ax.bar(labels, oranges, width, label='oranges', bottom=bananas, color='#ff8000')
-ax.bar(labels, peaches, width, label='peaches', bottom=oranges, color='#ffe5b4')
+# Heights of bars
+bars = np.add(apples, bananas).tolist()
+bars2 = np.add(bars, oranges).tolist()
+# The position of the bars on the x-axis
+r = [0, 1, 2]
 
+# Names of group and bar width
+names = ['Farrah', 'Fred', 'Felicia']
+width = 0.5
 
-# ax.set_xticks(labels)
-ax.set_yticks(range(0,90,10))
-ax.set(ylim=(0,80))
-ax.set_ylabel('Quantity of Fruit')
-ax.set_title('Number of Fruit per Person')
-ax.legend()
+# Create apples bar
+plt.bar(r, apples, width, color='red', label='apples')
+# Create bananas bars (middle), on top of the firs ones
+plt.bar(r, bananas, width, bottom=apples, color='yellow', label='bananas')
+# Create oranges bars
+plt.bar(r, oranges, width, bottom=bars, color='#ff8000', label='oranges')
+# create peaches bars (top)
+plt.bar(r, peaches, width, bottom=bars2, color='#ffe5b4', label='peaches')
+
+# Custom X axis
+plt.yticks(range(0, 90, 10))
+plt.xticks(r, names)
+plt.ylabel('Quantity of Fruit')
+plt.suptitle('Number of Fruit per Person')
+plt.legend()
+
+# Show graphic
 plt.show()
