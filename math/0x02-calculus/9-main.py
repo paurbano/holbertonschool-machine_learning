a52dccd87ed804b449b4118ccbc2af611b8b1ebf
Pablo <paurbano@gmail.com>
2020-07-24 21:14:59 +0000

    function that calculates sum i^2

---
+++ math/0x02-calculus/9-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+summation_i_squared = __import__('9-sum_total').summation_i_squared
+
+n = 3
+print(summation_i_squared(n))
+
+n = 2
+print(summation_i_squared(n))
+
+n = 5
+print(summation_i_squared(n))
+
+n = 'a'
+print(summation_i_squared(n))
+
+n = 5.5
+print(summation_i_squared(n))
+
+n = -5
+print(summation_i_squared(n))
---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' sum squares'''
+
+
+def summation_i_squared(n):
+    '''sum squares '''
+    sum = 0
+    if type(n) is not int or n < 0:
+        return None
+    if n == 1:
+        return 1
+
+    return (summation_i_squared(n-1) + n**2)
