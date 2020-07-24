1252d28b202bdbe1726d6e7977ffaa964fa1285b
Pablo <paurbano@gmail.com>
2020-07-24 22:00:18 +0000

    change condition < 1

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 def summation_i_squared(n):
     '''sum squares '''
     sum = 0
-    if type(n) is not int or n < 0:
+    if type(n) is not int or n < 1:
         return None
     if n == 1:
         return 1
