68cda03eb6b3e859df42286b341083efc387ea10
Pablo <paurbano@gmail.com>
2020-07-25 21:49:02 +0000

    calculates the integral of a polynomial

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''integrate '''
+import math
+
+
+def poly_integral(poly, C=0):
+    '''calculates the integral of a polynomial:'''
+    if not isinstance(poly, list) or poly is None or poly == []:
+        return None
+
+    if all(isinstance(i, (int, float)) for i in poly):
+        True
+    else:
+        return None
+    poly.insert(0, 0)
+    integral = [poly[i] / i for i in range(len(poly)) if i > 0]
+    del poly[0]
+    for coeff in range(len(integral)):
+        fraction, whole = math.modf(integral[coeff])
+        if fraction == 0:
+            integral[coeff] = round(integral[coeff])
+    integral.insert(0, 0)
+    return integral
---
+++ math/0x02-calculus/17-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+poly_integral = __import__('17-integrate').poly_integral
+
+poly = [5, 3, 0, 1]
+print(poly_integral(poly))
+
+poly = [2, 6, 3, 2, 4]
+print(poly_integral(poly))
+
+poly = [3,0,3]
+print(poly_integral(poly))
