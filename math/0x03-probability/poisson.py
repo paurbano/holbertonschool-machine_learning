223f2d57e3c513ac098ce7f874b2ab4d6bad9c4c
Pablo <paurbano@gmail.com>
2020-07-29 22:15:06 +0000

    update constructor

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -7,12 +7,11 @@ class Poisson():
     def __init__(self, data=None, lambtha=1.):
         ''' Constructor'''
         if data:
-            if type(data) != list:
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.data = data
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = float(sum(data) / len(data))
         else:
             if lambtha < 0:
                 raise ValueError('lambtha must be a positive value')
