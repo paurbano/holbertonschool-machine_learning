5a575e3c3045410020cfee1c2a1631c699ab432d
Pablo <paurbano@gmail.com>
2020-07-30 00:03:22 +0000

    interchange conditions in constructor

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,16 +6,16 @@ class Poisson():
     ''' Poisson'''
     def __init__(self, data=None, lambtha=1.):
         ''' Constructor'''
-        if data:
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
-        else:
-            if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
-            self.lambtha = float(lambtha)
 
     def pmf(self, k):
         '''Calculates PMF '''
