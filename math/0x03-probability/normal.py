e4666bc52c0ed8bb74a7c32834ba56f6f99ce7c5
Pablo <paurbano@gmail.com>
2020-07-30 15:01:10 +0000

    fix stddev condition constructor

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -7,12 +7,12 @@ class Normal():
     def __init__(self, data=None, mean=0., stddev=1.):
         '''constructor'''
         if data is None:
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError('stddev must be a positive value')
             self.mean = float(mean)
             self.stddev = float(stddev)
         else:
-            if type(data) is not list:
+            if not isinstance(data, list):
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
