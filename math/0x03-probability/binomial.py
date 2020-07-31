3edd74e74d614c42e806bcac2db5cd872e6cd078
Pablo <paurbano@gmail.com>
2020-07-31 03:39:12 +0000

    change condition for p on constructor

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -7,9 +7,9 @@ class Binomial():
     def __init__(self, data=None, n=1, p=0.5):
         '''constructor'''
         if data is None:
-            if n < 0:
+            if n <= 0:
                 raise ValueError('n must be a positive value')
-            if p < 0 and p > 1:
+            if p < 0 or p > 1:
                 raise ValueError('p must be greater than 0 and less than 1')
             self.n = int(n)
             self.p = float(p)
