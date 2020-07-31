d2a893ba40f7ee16fff8e319b3074a8d2133e89b
Pablo <paurbano@gmail.com>
2020-07-31 03:20:41 +0000

    constructor fix condition for p

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,7 +9,7 @@ class Binomial():
         if data is None:
             if n <= 0:
                 raise ValueError('n must be a positive value')
-            if p <= 0 and p > 1:
+            if p < 0 and p > 1:
                 raise ValueError('p must be greater than 0 and less than 1')
             self.n = int(n)
             self.p = float(p)
