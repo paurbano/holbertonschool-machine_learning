1aee3e6da92b700e1984a940e2c0be8eccba2994
Pablo <paurbano@gmail.com>
2020-08-06 04:55:33 +0000

    class Neuron with forward propagation

---
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+''' My first Neuron'''
+
+import numpy as np
+
+
+class Neuron():
+    '''class Neuron'''
+    def __init__(self, nx):
+        '''constructor'''
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''getter for Weigth'''
+        return self.__W
+
+    @property
+    def b(self):
+        '''getter for bias'''
+        return self.__b
+
+    @property
+    def A(self):
+        '''getter for activited function'''
+        return self.__A
+
+    def forward_prop(self, X):
+        '''Calculates the forward propagation of the neuron with sigmoid'''
+        x = np.matmul(self.__W, X) + self.__b
+        sigmoid = 1 / (1 + np.exp(-x))
+        self.__A = sigmoid
+        return self.__A
