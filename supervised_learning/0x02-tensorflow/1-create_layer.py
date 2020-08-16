7e35a1db402fff98e5e208bb9f0850b2ff8b3b35
Pablo <paurbano@gmail.com>
2020-08-16 03:57:59 +0000

    function Returns the tensor output of the layer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+''' create layer'''
+
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    '''create a layer
+        prev: tensor output of the previous layer
+        n: number of nodes in the layer to create
+        activation: activation function that the layer should use
+        Returns: the tensor output of the layer
+    '''
+    # tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    y = tf.layers.dense(prev, activation=activation, units=n, name='layer')
+    return y
