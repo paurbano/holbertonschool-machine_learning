d2ade310f79399b25646305f2d6d2ebec585464a
Pablo <paurbano@gmail.com>
2020-08-27 18:49:03 +0000

    L2 Regularization Cost tensorflow

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     '''L2 regularization'''
-    l2 = tf.contrib.layers.l2_regularizer(cost)
+    l2 = tf.losses.get_regularization_losses(cost)
     return l2
