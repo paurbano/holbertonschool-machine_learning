07b1d18dde4ad593661e8599adb331034b811d55
Pablo <paurbano@gmail.com>
2020-08-28 18:06:45 +0000

    fix pep8

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     '''L2 regularization'''
-    l2 =cost + tf.losses.get_regularization_losses()
+    l2 = cost + tf.losses.get_regularization_losses()
     return l2
