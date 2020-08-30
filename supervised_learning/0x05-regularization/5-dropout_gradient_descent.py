17e8ff0f24076ec0dae583c1eb73b16474d741a1
Pablo <paurbano@gmail.com>
2020-08-30 01:10:18 +0000

    Gradient Descent with Dropout

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -38,6 +38,6 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
 
         # update weights and bias
         weights['W'+str(i)] = weights['W'+str(i)] -\
-            (alpha * dW).T
+            (alpha * dW)
         weights['b'+str(i)] = weights['b'+str(i)] -\
             (alpha * db)
