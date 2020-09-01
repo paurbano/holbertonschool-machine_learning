13546430a72de01c94bfa145ed6c4a2f16806c49
Pablo <paurbano@gmail.com>
2020-09-01 14:27:02 +0000

    build neural network with keras Functional API

---
+++ supervised_learning/0x06-keras/1-input.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''build neural network with keras Functional API'''
+
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''builds a neural network with the Keras library:
+    Args:
+        nx: is the number of input features to the network
+        layers: is a list containing the number of nodes in each
+                layer of the network
+        activations: is a list containing the activation functions
+                    used for each layer of the network
+        lambtha: is the L2 regularization parameter
+        keep_prob: is the probability that a node will be kept for dropout
+
+    Returns: the keras model
+    '''
+    inputs = K.Input(shape=(nx,))
+    L2 = K.regularizers.l2(lambtha)
+    # Applies Dropout to the input.
+    dropout = K.layers.Dropout(1 - keep_prob)
+    dense = K.layers.Dense(layers[0], activation=activations[0],
+                       kernel_regularizer=L2)
+    x = dense(inputs)
+    x = K.layers.Dropout(1 - keep_prob)(x)
+    n = len(layers)
+    for i in range(1, n - 1):
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                           kernel_regularizer=L2)(x)
+        if i != n - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+    outputs = K.layers.Dense(layers[i + 1], activation=activations[i + 1],
+                             kernel_regularizer=L2)(x)
+    model = K.Model(inputs=inputs, outputs=outputs)
+    return model
