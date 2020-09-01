52a215ce201820bfc5d53f68fe8c6cd602084304
Pablo <paurbano@gmail.com>
2020-09-01 23:10:24 +0000

    Save and Load Model

---
+++ supervised_learning/0x06-keras/9-model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''Save and Load Model'''
+
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    '''saves an entire model:
+    Args:
+        network is the model to save
+        filename is the path of the file that the model should be saved to
+    Returns: None
+    '''
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    '''loads an entire model:
+        filename is the path of the file that the model should be loaded from
+        Returns: the loaded model
+    '''
+    model = K.odels.load_model(filename)
+    return model
