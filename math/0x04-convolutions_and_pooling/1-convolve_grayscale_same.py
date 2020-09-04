0b69343981c2e546b907be4189801cdd703dd1d3
Pablo <paurbano@gmail.com>
2020-09-04 16:27:37 +0000

    add padding

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''convolution on grayscale images'''
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    '''performs a valid convolution on grayscale images
+    Args:
+        images:array with shape (m, h, w) containing multiple grayscale images
+            m is the number of images
+            h is the height in pixels of the images
+            w is the width in pixels of the images
+        kernel: array with shape (kh, kw) containing the kernel
+            kh is the height of the kernel
+            kw is the width of the kernel
+        Return : a numpy.ndarray containing the convolved images
+    '''
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    n_h = h - kh + 1
+    n_w = w - kw + 1
+    # calculate padding
+    pad = int((kh - 1) / 2)
+    convolved = np.zeros([m, h, w])
+    # pad images
+    imagepaded = np.pad(images, pad_width=0, mode='constant',
+                        constant_values=0)
+    # Loop over every pixel of the output
+    for x in range(n_h):
+        for y in range(n_w):
+            # slice every image according to kernel size
+            image = imagepaded[:, x:x+kh, y:y+kw]
+            # element-wise multiplication of the kernel and the image
+            convolved[:, x, y] = np.multiply(image, kernel).sum(axis=(1, 2))
+    # Making sure output shape is correct
+    # assert(convolved.shape == (m, n_h, n_w))
+    return convolved
