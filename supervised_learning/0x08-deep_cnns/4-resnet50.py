7c8935e54d024a293e9f911ddf9829f084267172
Pablo <paurbano@gmail.com>
2020-09-17 21:32:54 +0000

    fix size AveragePool

--- supervised_learning/0x08-deep_cnns/4-resnet50.py
+++ supervised_learning/0x08-deep_cnns/4-resnet50.py
@@ -52,7 +52,7 @@ def resnet50():
     X = identity_block(X, [512, 512, 2048])
 
     # AVGPOOL.
-    X = K.layers.AveragePooling2D((2, 2))(X)
+    X = K.layers.AveragePooling2D((7, 7))(X)
 
     # output layer
     X = K.layers.Dense(1000, activation='softmax')(X)
