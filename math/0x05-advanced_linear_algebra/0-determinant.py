9fede9e3d8e79c8385dfe880dac1fc7773e2cb75
Pablo <paurbano@gmail.com>
2020-10-21 22:18:17 +0000

    calculates the determinant of a matrix

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''calculates the determinant of a matrix'''
+
+
+def determinant(matrix):
+    '''calculates the determinant of a matrix
+    Args:
+        matrix:  is a list of lists whose determinant should be calculated
+    Returns: the determinant of matrix
+    '''
+    if type(matrix) is not list or not matrix:
+        raise TypeError('matrix must be a list of lists')
+    for data in matrix:
+        if type(data) is not list:
+            raise TypeError('matrix must be a list of lists')
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            raise ValueError('matrix must be a square matrix')
+    # Section 1: Establish n parameter and copy A
+    n = len(matrix)
+    AM = matrix[:]  # copy_matrix(matrix)
+
+    # Section 2: Row manipulate A into an upper triangle matrix
+    for fd in range(n):  # fd stands for focus diagonal
+        # if AM[fd][fd] == 0:
+        #    AM[fd][fd] = 1.0e-18  # Cheating by adding zero + ~zero
+        for i in range(fd+1, n):  # skip row with fd in it.
+            crScaler = AM[i][fd] / AM[fd][fd]  # cr stands for "current row".
+            for j in range(n):  # cr - crScaler * fdRow, one element at a time.
+                AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
+
+    # Section 3: Once AM is in upper triangle form ...
+    product = 1
+    for i in range(n):
+        product *= AM[i][i]  # ... product of diagonals is determinant
+
+    return product
