2b32f13bd745109a9ca1136e173659185ce74a39
Pablo <paurbano@gmail.com>
2020-10-22 17:39:50 +0000

    fix style

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -22,7 +22,7 @@ def determinant(matrix):
     n = len(matrix)
     AM = matrix[:]
 
-    # Section 2: Row manipulate A into an upper triangle matrix
+    # Transform matrix into an upper triangle matrix
     for fd in range(n):  # fd stands for focus diagonal
         if AM[fd][fd] == 0:
             AM[fd][fd] = 1.0e-18  # Cheating by adding zero + ~zero
@@ -31,9 +31,10 @@ def determinant(matrix):
             for j in range(n):  # cr - crScaler * fdRow, one element at a time.
                 AM[i][j] = AM[i][j] - crScaler * AM[fd][j]
 
-    # Section 3: Once AM is in upper triangle form ...
+    # Once matrix is in upper triangle form ...
     product = 1
     for i in range(n):
-        product *= AM[i][i]  # ... product of diagonals is determinant
+        # product of diagonals is determinant
+        product *= AM[i][i]
 
     return round(product)
