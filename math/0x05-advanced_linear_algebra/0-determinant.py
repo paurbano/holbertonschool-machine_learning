745f03af85a0f23371a522d666baa026eb061fff
Pablo <paurbano@gmail.com>
2020-10-22 17:39:25 +0000

    fix style

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -25,7 +25,7 @@ def determinant(matrix):
     # Section 2: Row manipulate A into an upper triangle matrix
     for fd in range(n):  # fd stands for focus diagonal
         if AM[fd][fd] == 0:
-           AM[fd][fd] = 1.0e-18  # Cheating by adding zero + ~zero
+            AM[fd][fd] = 1.0e-18  # Cheating by adding zero + ~zero
         for i in range(fd+1, n):  # skip row with fd in it.
             crScaler = AM[i][fd] / AM[fd][fd]  # cr stands for "current row".
             for j in range(n):  # cr - crScaler * fdRow, one element at a time.
