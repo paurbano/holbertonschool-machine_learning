91188acc33c20cc25b020feb81c809cf5691f495
Pablo <paurbano@gmail.com>
2020-11-01 17:31:31 +0000

    fix validations

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -22,23 +22,31 @@ def marginal(x, n, P, Pr):
         Pr: is a 1D numpy.ndarray containing the prior beliefs of P
     Returns: the marginal probability of obtaining x and n
     '''
-    if type(n) is not int and n > 0:
-        raise ValueError('n must be a positive integer')
-    if type(x) is not int and x >= 0:
-        raise ValueError('x must be an integer that is greater than or\
-                         equal to 0')
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if type(x) is not int or x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+
     if x > n:
-        raise ValueError('x cannot be greater than n')
-    if type(P) is not np.ndarray:
-        raise TypeError('P must be a 1D numpy.ndarray')
-    if any(P < 0) or any(P > 1):
-        raise ValueError('All values in P must be in the range [0, 1]')
-    if type(Pr) is not np.ndarray and P.shape != Pr.shape:
-        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
-    if any(Pr < 0) or any(Pr > 1):
-        raise ValueError('All values in P must be in the range [0, 1]')
+        raise ValueError("x cannot be greater than n")
+
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if type(Pr) is not np.ndarray or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    if np.any(P > 1) or np.any(P < 0):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+
     if not np.isclose(Pr.sum(), 1):
-        raise ValueError('Pr must sum to 1')
+        raise ValueError("Pr must sum to 1")
+
     L = math.factorial(n) / (math.factorial(x) * math.factorial(n - x)) *\
         pow(P, x) * pow((1 - P), (n - x))
     return np.sum(L * Pr)
