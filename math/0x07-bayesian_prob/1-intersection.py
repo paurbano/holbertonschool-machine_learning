f6466530830b60d1c98bb6621f0ce0927788ef78
Pablo <paurbano@gmail.com>
2020-11-01 17:14:13 +0000

    fix validations

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -23,29 +23,30 @@ def intersection(x, n, P, Pr):
     Returns: a 1D numpy.ndarray containing the intersection of obtaining x and
             n with each probability in P, respectively
     '''
-    if type(n) is not int and n <= 0:
-        raise ValueError('n must be a positive integer')
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
 
-    if type(x) is not int and x <= 0:
-        raise ValueError('x must be an integer that is greater than or '
-                         'equal to 0')
+    if type(x) is not int or x <= 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+
     if x > n:
-        raise ValueError('x cannot be greater than n')
+        raise ValueError("x cannot be greater than n")
 
     if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError('P must be a 1D numpy.ndarray')
+        raise TypeError("P must be a 1D numpy.ndarray")
 
     if type(Pr) is not np.ndarray or P.shape != Pr.shape:
-        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if np.any(P < 0) or np.any(P > 1):
+    if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    if np.any(Pr < 0) or np.any(Pr > 1):
-        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if not np.isclose(Pr.sum(), 1):
-        raise ValueError('Pr must sum to 1')
+        raise ValueError("Pr must sum to 1")
 
     L = np.math.factorial(n) / (np.math.factorial(x) *
                                 np.math.factorial(n - x)) *\
