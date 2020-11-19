c2e41c97c10f0ca16c8a2f65892e1240d3849db4
Pablo <paurbano@gmail.com>
2020-11-19 17:31:00 +0000

    determines if a markov chain is absorbing

---
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env ptyhon3
+'''Absorbing Chains '''
+
+import numpy as np
+
+
+def absorbing(P):
+    '''determines if a markov chain is absorbing:
+    Args:
+        P: is a is a square 2D numpy.ndarray of shape (n, n) representing the
+            standard transition matrix
+           P[i, j] is the probability of transitioning from state i to state j
+           n is the number of states in the markov chain
+    Returns: True if it is absorbing, or False on failure
+    '''
+    if type(P) is not np.ndarray or len(P.shape) != 2 or\
+       P.shape[0] != P.shape[1]:
+        return None
+    if np.any(P < 0):
+        return None
+
+    if np.min(P ** 2) < 0 or np.min(P ** 3) < 0:
+        return None
+
+    P = P.copy()
+    absorb = np.ndarray(P.shape[0])
+    while True:
+        prev = absorb.copy()
+        absorb = np.any(P == 1, axis=0)
+        if absorb.all():
+            return True
+        if np.all(absorb == prev):
+            return False
+        absorbed = np.any(P[:, absorb], axis=1)
+        P[absorbed, absorbed] = 1
