510ddecc61dc828bc396fb5c5604636b2a3c195b
srdevemp <1205@holbertonschool.com>
2020-12-23 19:16:21 -0500

    change repo name

--- supervised_learning/0x10-nlp_metric/0-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-uni_bleu = __import__('0-uni_bleu').uni_bleu
-
-references = [["the", "cat", "is", "on", "the", "mat"], ["there", "is", "a", "cat", "on", "the", "mat"]]
-sentence = ["there", "is", "a", "cat", "here"]
-#references = [[['this', 'is', 'a', 'test'], ['this', 'is' 'test']]]
-#sentence = [['this', 'is', 'a', 'test']]
-print(uni_bleu(references, sentence))
