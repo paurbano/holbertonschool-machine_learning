3ebb9cb242e11f42d8150525b5f887a6daf1c148
srdevemp <1205@holbertonschool.com>
2021-01-07 18:11:21 -0500

    Fix V definition

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -23,7 +23,7 @@ class SelfAttention(tf.keras.layers.Layer):
         super().__init__()
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
-        self.V = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
         '''call method
