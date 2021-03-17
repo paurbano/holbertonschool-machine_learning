872eb369fa8beaf17b527eb4f39287252541b885
Pablo <paurbano@gmail.com>
2021-03-17 20:43:47 +0000

    ranks country origins of bands

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -1,5 +1,4 @@
 -- ranks country origins of bands
 SELECT origin, SUM(fans) AS nb_fans 
-FROM metal_bands 
-WHERE origin <> '' 
-GROUP BY origin HAVING nb_fans > 1 ORDER BY nb_fans DESC;
+FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
