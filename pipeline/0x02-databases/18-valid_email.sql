e1b6b06abb7543eb6cc31cb1abf3671ee957b4ae
Pablo <paurbano@gmail.com>
2021-03-18 03:08:53 +0000

    trigger resets the attribute valid_email

--- pipeline/0x02-databases/18-valid_email.sql
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -3,8 +3,8 @@ DELIMITER //
 CREATE TRIGGER reset_email BEFORE UPDATE ON users
 	FOR EACH ROW
 	BEGIN
-		IF STRCMP(OLD.email, NEW.email) <>0 THEN
-			UPDATE users SET valid_email = 0;
+		IF STRCMP(OLD.email, NEW.email) <> 0 THEN
+			SET NEW.valid_email = 0;
         END IF;
 	END//
 DELIMITER ;
